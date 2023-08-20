57c6cd533b040b468bea629916456f361e0db11d
Nkitina Ekaterina <52584077+katrinkattt@users.noreply.github.com>
2023-08-21 01:32:54 +0500

    mini fix

--- src/components/DataProfile.tsx
+++ src/components/DataProfile.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import {ScrollView, StyleSheet} from 'react-native';
-
+import {useSelector} from 'react-redux';
 import {s, vs} from 'react-native-size-matters';
 import SecondAuthInput from './common/SecondAutInput';
 
 export default function DataProfile() {
+  const user = useSelector(state => state.user);
   return (
     <ScrollView style={styles.card} showsVerticalScrollIndicator={false}>
       <SecondAuthInput
         label="Фамилия имя отчество*"
-        placeholder="Голованов Игорь Аркадьевич "
+        placeholder={user?.access_token.slice(1, 8) + '...token'}
         position="top"
       />
       <SecondAuthInput
         label="E-mail*"
-        placeholder="bystrov-vladimir@mail.ru"
+        placeholder={user?.email}
         position="center"
       />
       <SecondAuthInput
